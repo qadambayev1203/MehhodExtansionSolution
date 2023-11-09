70ff6682a3e7ead6212ad888c82b23c619fbf71a
qadambayev1203 <qadambayevmominjon003@gmail.com>
2023-11-09 16:57:15 +0500

    MethodExstansion

---
+++ MehhodExtansion/Mathextensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MehhodExtansion
+{
+    public static class Mathextensions
+    {
+        public static int Ayiruv(this Mathh math, int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ MehhodExtansion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MehhodExtansion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathh summ = new Mathh();
+            Console.WriteLine(summ.Summ(15, 12));
+            Console.WriteLine(summ.Ayiruv(15, 12));
+            Console.ReadKey();
+        }
+    }
+}
